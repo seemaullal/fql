798bfc32aa4bcfebbf78b81cf37034b41f265943
Seema <seemaullal@gmail.com>
2015-01-27 15:25:34 -0500

    Initial commit

---
+++ fql.js
@@ -0,0 +1,29 @@
+// Place your code here:
+
+// Adds properties of obj2 into obj1
+function merge(obj1, obj2) {
+}
+
+
+var FQL = function(table) {
+};
+
+FQL.prototype.exec = function () {};
+
+FQL.prototype.where = function (filters) {};
+
+FQL.prototype.count = function () {};
+
+FQL.prototype.limit = function (amount) {};
+
+FQL.prototype.select = function (columnNames) {};
+
+FQL.prototype.nimit = function () {};
+
+FQL.prototype.order = function (columnName) {};
+
+FQL.prototype.left_join = function (foriegnFql, rowMatcher) {};
+
+FQL.prototype.addIndex = function (columnName) {};
+
+FQL.prototype.getInidicesOf = function (columnName, val) {};
