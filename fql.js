de5e167ca13e90d382126e74f08cb240f1184423
Seema <seemaullal@gmail.com>
2015-01-27 15:38:16 -0500

    Added merge function to combine 2 objects

--- fql.js
+++ fql.js
@@ -2,6 +2,18 @@
 
 // Adds properties of obj2 into obj1
 function merge(obj1, obj2) {
+
+	var tmp = { };
+	for (atr in obj1) {
+		if (obj1.hasOwnProperty(atr)) 
+			tmp[atr] = obj1[atr];
+	}
+
+	for (atr2 in obj2) {
+		if (obj2.hasOwnProperty(atr2)) 
+			tmp[atr2] = obj2[atr2];
+	}
+	return tmp;
 }
 
 
